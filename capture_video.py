799f766335fc7fd8dcf0f28b8a798172ad7cb7ba
Bent Prellberg <bent@flucto.tech>
2025-07-31 15:29:25 +0200

    implemented a robust recording pipeline

--- capture_video.py
+++ capture_video.py
@@ -4,7 +4,22 @@ from pathlib import Path
 from pypylon import pylon
 import time
 
-def record_video(cam:Camera, output_path: str) -> str:
+
+def build_camera(autoexposure,contrast_mode,contrast,exposure,gain,width,height,fps):
+    cam = (
+        Camera()
+        .open()
+        .set_auto_exposure(autoexposure)
+        .set_contrast_mode(contrast_mode)
+        .set_contrast(contrast)
+        .set_exposure_time(exposure)
+        .set_gain(gain)
+        .set_image_size(width, height)
+        .set_frame_rate(fps)
+    )
+    return cam
+
+def set_up_writer(cam:Camera, output_path):
 
     print(f"ACTUAL CAMERA FRAMERATE {cam.get_frame_rate()}")
     print(f"INTERNAL TEMPERATURE {cam.get_temperature()}")
@@ -12,17 +27,20 @@ def record_video(cam:Camera, output_path: str) -> str:
     height = cam.params.get('height')
 
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+    print(output_path)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    if not output_path.lower().endswith('.mp4'):
-        output_path = f"{Path(output_path).stem}.mp4"
     writer = cv2.VideoWriter(output_path, fourcc, cam.get_frame_rate(), (width, height), True)
     if not writer.isOpened():
         raise RuntimeError(f"VideoWriter could not be opened: {output_path}")
 
+    return writer
+
+def record_video(cam:Camera, segment_duration, writer):
+
     cam.cam.StartGrabbing(pylon.GrabStrategy_OneByOne)
     t_0 = time.time()
     try:
-        while True:
+        while time.time() - t_0 < segment_duration:
             grabResult = cam.cam.RetrieveResult(1000, pylon.TimeoutHandling_ThrowException)
             if grabResult.GrabSucceeded():
                 frame = grabResult.Array
@@ -31,14 +49,22 @@ def record_video(cam:Camera, output_path: str) -> str:
                 writer.write(frame)
             grabResult.Release()
             grabResult.Release()
+
+        if cam.cam.IsGrabbing():
+            cam.cam.StopGrabbing()
+        writer.release()
+        cam.close()
+
     except KeyboardInterrupt:
         video_length = time.time() - t_0
         print(f"Stopped by Interrupt - Video Length: {video_length} s")
         print(f"INTERNAL TEMPERATURE {cam.get_temperature()}")
 
+    """
     finally:
         if cam.cam.IsGrabbing():
             cam.cam.StopGrabbing()
         writer.release()
         cam.close()
         return output_path
+    """
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from capture_video import record_video
+from capture_video import set_up_writer,record_video
 from capture_camera import Camera
 from pathlib import Path
 import yaml
+from datetime import datetime
 
 def load_config(config_path: str = "config.yaml") -> dict:
     if not Path(config_path).is_file():
@@ -16,7 +17,6 @@ if __name__ == "__main__":
         video_cfg = cfg.get('video', {})
 
 
-        output_path = video_cfg.get('out', 'data/video/output.mp4')
         fps = video_cfg.get('fps', 30)
         width = video_cfg.get('width', 1280)
         height = video_cfg.get('height', 720)
@@ -25,22 +25,27 @@ if __name__ == "__main__":
         autoexposure = video_cfg.get('auto_exposure', 'Continuous')
         contrast_mode = video_cfg.get('contrast_mode', 'Linear')
         contrast = video_cfg.get('contrast',1.1)
-
-        cam = (
-            Camera()
-            .open()
-            .set_auto_exposure(autoexposure)
-            .set_contrast_mode(contrast_mode)
-            .set_contrast(contrast)
-            .set_exposure_time(exposure)
-            .set_gain(gain)
-            .set_image_size(width, height)
-            .set_frame_rate(fps)
-        )
+        segment_duration = 5000 #in seconds
 
 
-        output = record_video(cam,output_path)
-        print(f"Video saved to: {output_path}")
+
+        while True:
+            cam = (
+                Camera()
+                .open()
+                .set_auto_exposure(autoexposure)
+                .set_contrast_mode(contrast_mode)
+                .set_contrast(contrast)
+                .set_exposure_time(exposure)
+                .set_gain(gain)
+                .set_image_size(width, height)
+                .set_frame_rate(fps)
+            )
+
+            output_path = (Path(video_cfg["output_dir"]) / f"basler_{datetime.now().strftime(video_cfg["format"])}.mp4")
+            writer = set_up_writer(cam, output_path)
+            output = record_video(cam, segment_duration, writer)
+            print(f"Video saved to: {output_path}")
 
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
